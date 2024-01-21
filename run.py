1526cf0de8eff39329af48b069997c5b8a443a16
Mikołaj Burdzy <m.burdzy@odo24.pl>
2024-01-21 02:09:27 +0100

    Major changes

--- run.py
+++ run.py
@@ -1,22 +1,26 @@
 import json
 import redis
 import ass
+import sys
 from flask import Flask, jsonify
 import threading
 import requests
 import time
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash import Dash, dcc, html, Output, Input
 import plotly.express as px
 import pandas as pd
 import random
-from math import log
+import math
 
 app = Flask(__name__)
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+redis_client.flushdb()
 epochTime = time.time()
 MAX_RECORDS = 600
 MAX_IDS = 6
+redBoxes = [[{'type': 'rect', 'x0': 40, 'x1': 46, 'y0': 0, 'y1': 1050, 'fillcolor': 'rgba(255, 0, 0, 0.2)', 'line': {'width': 0}},] for _ in range(6)]
+num = 30
 
 # Initialize Dash app
 dash_app = Dash(__name__, server=app)
@@ -28,7 +32,6 @@ def async_request():
         try:
             for i in range(1,7):
                 url = 'http://tesla.iem.pw.edu.pl:9080/v2/monitor/'+str(i)
-                # print(time.time() - epochTime)
                 response = requests.get(url)
                 data = response.json()
 
@@ -79,8 +82,20 @@ dash_app.layout = html.Div(children=[
         value5='5555',
         value6='6666'
     ),
+    dcc.Slider(
+        id='my-slider',
+        min=30,
+        max=600,
+        step=1,
+        marks={i: str(i) for i in range(30, 601, 50)},  # optional: add marks for better visualization
+        value=30  # default value
+    ),
     dcc.Graph(id='line-plot-1'),
     dcc.Graph(id='line-plot-2'),
+    dcc.Graph(id='line-plot-3'),
+    dcc.Graph(id='line-plot-4'),
+    dcc.Graph(id='line-plot-5'),
+    dcc.Graph(id='line-plot-6'),
 
     dcc.Interval(
         id='interval-component',
@@ -94,45 +109,63 @@ dash_app.layout = html.Div(children=[
     [Output('nameAndYear', 'children'),
      Output('line-plot-1', 'figure'),
      Output('line-plot-2', 'figure'),
+     Output('line-plot-3', 'figure'),
+     Output('line-plot-4', 'figure'),
+     Output('line-plot-5', 'figure'),
+     Output('line-plot-6', 'figure'),
      Output('feetComponent', 'value1'),
-     Output('feetComponent', 'value2'),
      Output('feetComponent', 'value3'),
-     Output('feetComponent', 'value4'),
      Output('feetComponent', 'value5'),
+     Output('feetComponent', 'value2'),
+     Output('feetComponent', 'value4'),
      Output('feetComponent', 'value6')],
-    [Input('interval-component', 'n_intervals')]
+    [Input('interval-component', 'n_intervals'),
+     State('my-slider', 'value')]
 )
-def update_data(n_intervals):
+def update_data(n_intervals, num_of_points):
     rand = 2 # random.randint(1, 6)
     records = get_records(rand)
-    record = records[0]
-    birthdate = record.get("birthdate")
-    firstname = record.get("firstname")
-    lastname = record.get("lastname")
+    if records is not None:
+        record = records[0]
+        birthdate = record.get("birthdate")
+        firstname = record.get("firstname")
+        lastname = record.get("lastname")
+        sensorNames = [value.get("name") for value in record.get("trace").get("sensors")]
+        global num 
 
-    time_series_data_1 = [value.get("trace").get("sensors")[0].get("value") for value in records]
-    time_series_data_2 = [value.get("trace").get("sensors")[1].get("value") for value in records]
-    time_series_data_3 = [value.get("trace").get("sensors")[2].get("value") for value in records]
-
-    time_series_data_4 = [value.get("trace").get("sensors")[3].get("value") for value in records]
-    time_series_data_5 = [value.get("trace").get("sensors")[4].get("value") for value in records]
-    time_series_data_6 = [value.get("trace").get("sensors")[5].get("value") for value in records]
-
-    # Create traces for line plots
-    trace1 = dict(x=list(range(len(time_series_data_1))), y=time_series_data_1[::-1], mode='lines', name='Line 1')
-    trace2 = dict(x=list(range(len(time_series_data_2))), y=time_series_data_2[::-1], mode='lines', name='Line 2')
-    trace3 = dict(x=list(range(len(time_series_data_3))), y=time_series_data_3[::-1], mode='lines', name='Line 3')
-
-    trace4 = dict(x=list(range(len(time_series_data_4))), y=time_series_data_4[::-1], mode='lines', name='Line 4')
-    trace5 = dict(x=list(range(len(time_series_data_5))), y=time_series_data_5[::-1], mode='lines', name='Line 5')
-    trace6 = dict(x=list(range(len(time_series_data_6))), y=time_series_data_6[::-1], mode='lines', name='Line 6')
-
-    # Create figures for line plots
-    figure1 = {'data': [trace1, trace2, trace3], 'layout': {'title': 'Line Plot 1', 'yaxis_type': 'log'}}
-    figure2 = {'data': [trace4, trace5, trace6], 'layout': {'title': 'Line Plot 2', 'yaxis_type': 'log'}}
-
-    return firstname + ' ' + lastname + ' ' + birthdate, figure1, figure2, time_series_data_1[0], time_series_data_2[0], time_series_data_3[0], time_series_data_4[0], time_series_data_5[0], time_series_data_6[0]
-
+        time_series_data = [[] for _ in range(6)]
+        time_series_anomalies = [[] for _ in range(6)]
+        traces = [dict() for _ in range(6)]
+        figures = [dict() for _ in range(6)]
+        for i in range(0,6):
+                # [{'type': 'rect', 'x0': 2, 'x1': 4, 'y0': 0, 'y1': 1050, 'fillcolor': 'rgba(255, 0, 0, 0.2)', 'line': {'width': 0}},
+                # {'type': 'rect', 'x0': 2, 'x1': 4, 'y0': 0, 'y1': 1050, 'fillcolor': 'rgba(255, 0, 0, 0.2)', 'line': {'width': 0}},
+                # {'type': 'rect', 'x0': 2, 'x1': 4, 'y0': 0, 'y1': 1050, 'fillcolor': 'rgba(255, 0, 0, 0.2)', 'line': {'width': 0}}]
+            
+            time_series_data[i] = [value.get("trace").get("sensors")[i].get("value") for value in records]
+            time_series_anomalies[i] = [value.get("trace").get("sensors")[i].get("anomaly") for value in records]
+            traces[i] = dict(y=time_series_data[i][::-1][-num_of_points:], mode='lines', name=sensorNames[i], line = {'color': 'blue'})
+            previousLimit = min(len(time_series_data[i]),num)
+            currentLimit = min(len(time_series_data[i]),num_of_points)
+            if num > num_of_points:
+                shiftToLeft = previousLimit - currentLimit
+                for refBoxDict in redBoxes[i]:
+                    refBoxDict['x0'] = refBoxDict['x0'] - shiftToLeft
+                    refBoxDict['x1'] = refBoxDict['x1'] - shiftToLeft
+            elif num < num_of_points:
+                for refBoxDict in redBoxes[i]:
+                    refBoxDict['x0'] = refBoxDict['x0'] - len(time_series_data[i]) + num_of_points
+                    refBoxDict['x1'] = refBoxDict['x1'] - len(time_series_data[i]) + num_of_points
+            if(time_series_anomalies[i][0]):
+                redBoxes[i].append({'type': 'rect', 'x0': len(time_series_data[i]), 'x1': len(time_series_data[i])+2, 'y0': 0, 'y1': 1050, 'fillcolor': 'rgba(255, 0, 0, 0.6)', 'line': {'width': 0}})
+            figures[i] = {'data': [traces[i]], 'layout': {'title': sensorNames[i],'shapes': redBoxes[i]}}
+        if num != num_of_points:
+            num = num_of_points
+            
+        return firstname + ' ' + lastname + ' ' + birthdate, figures[0], figures[1], figures[2], figures[3], figures[4], figures[5],  time_series_data[0][0], time_series_data[1][0], time_series_data[2][0], time_series_data[3][0], time_series_data[4][0], time_series_data[5][0]
+    else:
+        return None
+    
 def get_records(record_id):
     records = get_records_by_id(record_id)
     return records
